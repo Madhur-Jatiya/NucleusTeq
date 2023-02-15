8a1789ca4df8bac166f59a287857646aa35d33ec
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-15 19:03:32 +0530

    DivideTwoNumbers

--- Basic Java Task 1/DivideTwoNumbers.java
+++ Basic Java Task 1/DivideTwoNumbers.java
@@ -1,6 +1,17 @@
-public class PrintName {
+import java.util.Scanner;
+
+public class DivideTwoNumbers {
     public static void main(String[] args) {
-        System.out.println("Hello");
-        System.out.println("My name is Madhur Jatiya");
+        Scanner input = new Scanner(System.in);
+        
+        System.out.print("Enter the first number: ");
+        double num1 = input.nextDouble();
+        
+        System.out.print("Enter the second number: ");
+        double num2 = input.nextDouble();
+        
+        double result = num1 / num2;
+        
+        System.out.println(num1 + " / " + num2 + " = " + result);
     }
 }
