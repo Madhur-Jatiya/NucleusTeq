47221c0de57512c58ca857a49dba29d102ce3ac9
Madhurmoms <madhurjatiya13@gmail.com>
2023-02-22 12:33:21 +0530

    RupeeTODollar

---
+++ Task 2 (by priyanka)/RupeeTODollar.java
@@ -0,0 +1,10 @@
+import java.util.*;
+public class RupeeTODollar {
+    public static void main(String[] args) {
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter Rupees : ");
+        float Rupees = sc.nextInt();
+        
+        System.out.println("Dollar = $" + Rupees/82.84);
+    }
+}
