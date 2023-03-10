79ff0183d16d8c8c813a730d5d392bdaa0afc128
Madhur <madhurmoms@gmail.com>
2023-03-10 14:41:22 +0530

    buffer reader

--- Generic, Exception, File Handling/TheFileHandling/src/BufferReaderExample/BufferReaderClass2.java
+++ Generic, Exception, File Handling/TheFileHandling/src/BufferReaderExample/BufferReaderClass2.java
@@ -3,19 +3,19 @@ package BufferReaderExample;
 import java.io.BufferedReader;
 import java.io.FileReader;
 
-public class BufferReaderClass {
+public class BufferReaderClass2 {
 
     public static void main(String[] args) {
         try {
             FileReader fileReader = new FileReader("files\\BufferedWriter.txt");
             BufferedReader bufferedReader = new BufferedReader(fileReader);
 
-            String line = bufferedReader.readLine();
-            while (line != null) {
-                System.out.println(line);
-                line = bufferedReader.readLine();
+            int data = bufferedReader.read();
+            while (data != -1) {
+                System.out.print((char) data);
+                data = bufferedReader.read();
             }
-
+            System.out.println("");
         } catch (Exception e) {
         }
     }
