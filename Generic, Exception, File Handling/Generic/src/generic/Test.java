c2f2b619149fb640114db963f876ebadc35913d7
Madhur <madhurmoms@gmail.com>
2023-03-10 18:08:16 +0530

    generic code

--- Generic, Exception, File Handling/Generic/src/generic/Test.java
+++ Generic, Exception, File Handling/Generic/src/generic/Test.java
@@ -5,5 +5,17 @@ public class Test {
     public static void main(String[] args) {
         Generic<Integer> generic = new Generic<Integer>(12345);
         System.out.println(generic.getTemp());
+
+        Generic<String> generic2 = new Generic<String>("Madhur");
+        System.out.println(generic2.getTemp());
+
+        Generic<Character> generic3 = new Generic<Character>('A');
+        System.out.println(generic3.getTemp());
+
+        Generic<Double> generic4 = new Generic<Double>(345.225);
+        System.out.println(generic4.getTemp());
+
+        Generic<Boolean> generic5 = new Generic<Boolean>(true);
+        System.out.println(generic5.getTemp());
     }
 }
