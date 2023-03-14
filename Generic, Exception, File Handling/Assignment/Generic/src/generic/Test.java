e5a550f1d718653340ab235ea56896aaf145686a
Madhur <madhurmoms@gmail.com>
2023-03-14 17:37:25 +0530

    emp

--- Generic, Exception, File Handling/Assignment/Generic/src/generic/Test.java
+++ Generic, Exception, File Handling/Assignment/Generic/src/generic/Test.java
@@ -3,19 +3,19 @@ package generic;
 public class Test {
 
     public static void main(String[] args) {
-        Generic<Integer> generic = new Generic<Integer>(12345);
+        Generic<Integer> generic = new Generic<>(12345);
         System.out.println(generic.getTemp());
 
-        Generic<String> generic2 = new Generic<String>("Madhur");
+        Generic<String> generic2 = new Generic<>("Madhur");
         System.out.println(generic2.getTemp());
 
-        Generic<Character> generic3 = new Generic<Character>('A');
+        Generic<Character> generic3 = new Generic<>('A');
         System.out.println(generic3.getTemp());
 
-        Generic<Double> generic4 = new Generic<Double>(345.225);
+        Generic<Double> generic4 = new Generic<>(345.225);
         System.out.println(generic4.getTemp());
 
-        Generic<Boolean> generic5 = new Generic<Boolean>(true);
+        Generic<Boolean> generic5 = new Generic<>(true);
         System.out.println(generic5.getTemp());
     }
 }
