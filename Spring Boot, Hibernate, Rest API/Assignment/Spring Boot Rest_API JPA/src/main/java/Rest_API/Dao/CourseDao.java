79873a7f5d062d04ce975bb171a2230ff45f9138
Madhur <madhurmoms@gmail.com>
2023-05-12 10:08:17 +0530

    name

--- Spring Boot, Hibernate, Rest API/Assignment/Spring Boot Rest_API JPA/src/main/java/Rest_API/Dao/CourseDao.java
+++
@@ -1,9 +0,0 @@
-package Rest_API.Dao;
-
-import org.springframework.data.jpa.repository.JpaRepository;
-
-import Rest_API.entity.Course;
-
-public interface CourseDao extends JpaRepository<Course, Integer>{
-
-}
